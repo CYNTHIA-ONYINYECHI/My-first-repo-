a57efa9221380cbacb4378dba68e674d7848b182
CYNTHIA-ONYINYECHI <cynthiadamian22@gmail.com>
2023-09-25 23:30:42 +0100

    my first commit

---
+++ login.js
@@ -0,0 +1,15 @@
+//checking
+function check() {
+    var storedName = localStorage.getItem('name');
+    var storedPw = localStorage.getItem('pw');
+
+    var userName = document.getElementById('userName');
+    var userPw = document.getElementById('userPw');
+    var userRemember = document.getElementById("rememberMe");
+
+    if (userName.value == storedName && userPw.value == storedPw) {
+        window.open('index.html');
+    } else {
+        alert('Error on login');
+    }
+}
